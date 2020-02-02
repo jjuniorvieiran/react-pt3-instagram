04aa2bfdfe047e1fc5bb08999f7833742fa0d9b3
Junior <junior.jaava@gmail.com>
2020-02-02 18:01:36 +0100

    decoupled Timeline from LogicaTimeline. Now the publish and subscribe are in LogicaTimeline

--- projeto-instalura-curso-aula5/src/componentes/Timeline.js
+++ projeto-instalura-curso-aula5/src/componentes/Timeline.js
@@ -1,97 +1,66 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
-import Pubsub from 'pubsub-js';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
-import LogicaTimeline from '../logicas/LogicaTimeline' 
+import LogicaTimeline from '../logicas/LogicaTimeline'
 
 export default class Timeline extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {fotos:[]};
-      this.login = this.props.login;
-      this.logicaTimeline = new LogicaTimeline([]);
+  constructor(props) {
+    super(props);
+    this.state = { fotos: [] };
+    this.login = this.props.login;
+    this.logicaTimeline = new LogicaTimeline([]);
+  }
+
+  componentWillMount() {
+    this.logicaTimeline.subscribe(fotos => {
+      this.setState({ fotos });
+    })
+  }
+
+  carregaFotos() {
+    let urlPerfil;
+
+    if (this.login === undefined) {
+      urlPerfil = `https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+    } else {
+      urlPerfil = `https://instalura-api.herokuapp.com/api/public/fotos/${this.login}`;
     }
+    this.logicaTimeline.lista(urlPerfil)
+  }
 
-    componentWillMount(){
-      Pubsub.subscribe('timeline',(topico,fotos) => {
-        this.setState({fotos});
-      });
+  componentDidMount() {
+    this.carregaFotos();
+  }
 
-      Pubsub.subscribe('novos-comentarios',(topico,infoComentario) => {
-        const fotoAchada = this.state.fotos.find(foto => foto.id === infoComentario.fotoId);        
-        fotoAchada.comentarios.push(infoComentario.novoComentario);
-        this.setState({fotos:this.state.fotos});        
-      });      
-    }
-
-    carregaFotos(){  
-      let urlPerfil;
-
-      if(this.login === undefined) {
-        urlPerfil = `https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
-      } else {
-        urlPerfil = `https://instalura-api.herokuapp.com/api/public/fotos/${this.login}`;
-      }
-
-      fetch(urlPerfil)
-       .then(response => response.json())
-       .then(fotos => {         
-         this.setState({fotos:fotos});
-         this.logicaTimeline = new LogicaTimeline(fotos);
-       });      
-    }
-
-    componentDidMount(){
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.login !== undefined) {
+      this.login = nextProps.login;
       this.carregaFotos();
     }
+  }
 
-    componentWillReceiveProps(nextProps){
-      if(nextProps.login !== undefined){
-        this.login = nextProps.login;
-        this.carregaFotos();
-      }
-    }
+  like(fotoId) {
+    this.logicaTimeline.like(fotoId);
+  }
 
-    like(fotoId) {
-      this.logicaTimeline.like(fotoId);
-    }
+  comenta(fotoId, textoComentario) {
+    this.logicaTimeline.comenta(fotoId, textoComentario);
+  }
 
-    comenta(fotoId,textoComentario) {
-      const requestInfo = {
-        method:'POST',
-        body:JSON.stringify({texto:textoComentario}),
-        headers: new Headers({
-          'Content-type':'application/json'
-        })
-      };
-
-      fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,requestInfo)
-        .then(response => {
-          if(response.ok){
-            return response.json();
-          } else {
-            throw new Error("não foi possível comentar");
-          }
-        })
-        .then(novoComentario => {
-          Pubsub.publish('novos-comentarios',{fotoId,novoComentario});
-        });      
-    }
-
-    render(){
-        return (
-        <div className="fotos container">
+  render() {
+    return (
+      <div className="fotos container">
         <ReactCSSTransitionGroup
           transitionName="timeline"
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
-            {
-              this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta}/>)
-            }               
-        </ReactCSSTransitionGroup>        
- 
-        </div>            
-        );
-    }
+          {
+            this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)} />)
+          }
+        </ReactCSSTransitionGroup>
+
+      </div>
+    );
+  }
 }
--- projeto-instalura-curso-aula5/src/logicas/LogicaTimeline.js
+++ projeto-instalura-curso-aula5/src/logicas/LogicaTimeline.js
@@ -6,6 +6,39 @@ export default class LogicaTimeline {
     this.fotos = fotos;
   }
 
+  lista(urlPerfil) {
+    fetch(urlPerfil)
+      .then(response => response.json())
+      .then(fotos => {
+        this.fotos = fotos;
+        Pubsub.publish('timeline', this.fotos);
+      });
+  }
+
+  comenta(fotoId, textoComentario) {
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({ texto: textoComentario }),
+      headers: new Headers({
+        'Content-type': 'application/json'
+      })
+    };
+
+    fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("não foi possível comentar");
+        }
+      })
+      .then(novoComentario => {
+        const fotoAchada = this.fotos.find(foto => foto.id === fotoId);
+        fotoAchada.comentarios.push(novoComentario);
+        Pubsub.publish('timeline', this.fotos);
+      });
+  }
+
   like(fotoId) {
     fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, { method: 'POST' })
       .then(response => {
@@ -18,7 +51,7 @@ export default class LogicaTimeline {
       .then(liker => {
         const fotoAchada = this.fotos.find(foto => foto.id === fotoId);
         fotoAchada.likeada = !fotoAchada.likeada;
-  
+
         const possivelLiker = fotoAchada.likers.find(likerAtual => likerAtual.login === liker.login);
 
         if (possivelLiker === undefined) {
@@ -27,8 +60,13 @@ export default class LogicaTimeline {
           const novosLikers = fotoAchada.likers.filter(likerAtual => likerAtual.login !== liker.login);
           fotoAchada.likers = novosLikers;
         }
-        Pubsub.publish('timeline',this.fotos);
-      }
-      )
+        Pubsub.publish('timeline', this.fotos);
+      })
+  }
+
+  subscribe(callback) {
+    Pubsub.subscribe('timeline', (topico, fotos) => {
+      callback(fotos);
+    })
   }
 }
