9b1d8262aabe1ae2784f8d9a2bfab42a5bc28063
Junior <junior.jaava@gmail.com>
2020-02-08 15:01:11 +0100

    Migrate all function to use redux: like, commant and list photo

--- projeto-instalura-curso-aula5/src/logicas/TimelineApi.js
+++ projeto-instalura-curso-aula5/src/logicas/TimelineApi.js
@@ -1,13 +1,5 @@
-import Pubsub from 'pubsub-js';
-
 export default class TimelineApi {
-
-  constructor(fotos) {
-    this.fotos = fotos;
-  }
-
-  //instead of execute the list. we pass the function to dispatch
-  static lista(urlPerfil) {
+  static lista(urlPerfil) { //instead of execute the list. we pass the function to dispatch
     return dispatch => {
       fetch(urlPerfil)
         .then(response => response.json())
@@ -18,58 +10,44 @@ export default class TimelineApi {
     }
   }
 
-  comenta(fotoId, textoComentario) {
-    const requestInfo = {
-      method: 'POST',
-      body: JSON.stringify({ texto: textoComentario }),
-      headers: new Headers({
-        'Content-type': 'application/json'
-      })
-    };
-
-    fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("não foi possível comentar");
-        }
-      })
-      .then(novoComentario => {
-        const fotoAchada = this.fotos.find(foto => foto.id === fotoId);
-        fotoAchada.comentarios.push(novoComentario);
-        Pubsub.publish('timeline', this.fotos);
-      });
+  static comenta(fotoId, textoComentario) {
+    return dispatch => {
+      const requestInfo = {
+        method: 'POST',
+        body: JSON.stringify({ texto: textoComentario }),
+        headers: new Headers({
+          'Content-type': 'application/json'
+        })
+      };
+      fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("não foi possível comentar");
+          }
+        })
+        .then(novoComentario => {
+          dispatch({ type: 'COMENTARIO', fotoId, novoComentario });
+          return novoComentario;
+        });
+    }
   }
 
-  like(fotoId) {
-    fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, { method: 'POST' })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("não foi possível realizar o like da foto");
-        }
-      })
-      .then(liker => {
-        const fotoAchada = this.fotos.find(foto => foto.id === fotoId);
-        fotoAchada.likeada = !fotoAchada.likeada;
-
-        const possivelLiker = fotoAchada.likers.find(likerAtual => likerAtual.login === liker.login);
-
-        if (possivelLiker === undefined) {
-          fotoAchada.likers.push(liker);
-        } else {
-          const novosLikers = fotoAchada.likers.filter(likerAtual => likerAtual.login !== liker.login);
-          fotoAchada.likers = novosLikers;
-        }
-        Pubsub.publish('timeline', this.fotos);
-      })
-  }
-
-  subscribe(callback) {
-    Pubsub.subscribe('timeline', (topico, fotos) => {
-      callback(fotos);
-    })
+  static like(fotoId) {
+    return dispatch => {
+      fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, { method: 'POST' })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("não foi possível realizar o like da foto");
+          }
+        })
+        .then(liker => {
+          dispatch({ type: 'LIKE', fotoId, liker });
+          return liker;
+        })
+    }
   }
 }
