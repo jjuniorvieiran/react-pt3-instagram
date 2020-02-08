2ef18bbf5de0d3799043fa60df8eb5be29f471fd
Junior <junior.jaava@gmail.com>
2020-02-08 17:01:59 +0100

    feature like and comment immutable; passing function as parameter callback

--- projeto-instalura-curso-aula5/src/reducers/timeline.js
+++ projeto-instalura-curso-aula5/src/reducers/timeline.js
@@ -1,42 +1,45 @@
 import { List } from 'immutable';
 
+function trocaFoto(lista, fotoId, callbackAtualizaPropriedades) {
+
+    const fotoEstadoAntigo = lista.find(foto => foto.id === fotoId);
+    const novasPropriedades = callbackAtualizaPropriedades(fotoEstadoAntigo)
+
+    const fotoEstadoNovo = Object.assign({}, fotoEstadoAntigo, novasPropriedades);
+    const indiceDaLista = lista.findIndex(foto => foto.id === fotoId);
+
+    return lista.set(indiceDaLista, fotoEstadoNovo);
+}
+
 export function timeline(state = new List(), action) { //initial state empty EC6
     if (action.type === 'LISTAGEM') {
         return new List(action.fotos);
     }
 
     if (action.type === 'COMENTARIO') {
-        const fotoEstadoAntigo = state.find(foto => foto.id === action.fotoId);
-        const novosComentarios = fotoEstadoAntigo.comentarios.concat(action.novoComentario);
-    
-        //Nós queremos que ele preencha com todas propriedade que tem no fotoEstadoAtual e depois, que troque a propriedade comentarios 
-        //pelos novosComentarios. Sempre o último objeto literal, prevalecerá a que estava antes. Isto irá nos retornar o fotoEstadoNovo.
-        const fotoEstadoNovo = Object.assign({},fotoEstadoAntigo,{comentarios:novosComentarios});
-    
-        const indiceDaLista = state.findIndex(foto => foto.id === action.fotoId);
-        //O próximo passo será o adicionar a variável novaLista com o state.set() para trocarmos um elemento com determinado índice. 
-        //Incluiremos dois parâmetros: indiceDaLista e fotoEstadoNovo. No fim, será retornado uma novaLista.
-        const novaLista = state.set(indiceDaLista,fotoEstadoNovo);
-    
-        return novaLista;
+        return trocaFoto(state, action.fotoId, fotoEstadoAntigo => {
+            const novosComentarios = fotoEstadoAntigo.comentarios.concat(action.novoComentario);
+            return { comentarios: novosComentarios };
+        });
     }
 
     if (action.type === 'LIKE') {
-        const fotoAchada = state.find(foto => foto.id === action.fotoId);
-        fotoAchada.likeada = !fotoAchada.likeada;
+        return trocaFoto(state, action.fotoId, fotoEstadoAntigo => {
+            const likeada = !fotoEstadoAntigo.likeada;
 
-        const liker = action.liker;
+            const liker = action.liker;
+            const possivelLiker = fotoEstadoAntigo.likers.find(likerAtual => likerAtual.login === liker.login);
 
-        const possivelLiker = fotoAchada.likers.find(likerAtual => likerAtual.login === liker.login);
+            let novosLikers;
+            if (possivelLiker === undefined) {
+                novosLikers = fotoEstadoAntigo.likers.concat(liker);
+            } else {
+                novosLikers = fotoEstadoAntigo.likers.filter(likerAtual => likerAtual.login !== liker.login);
+            }
 
-        if (possivelLiker === undefined) {
-            fotoAchada.likers.push(liker);
-        } else {
-            const novosLikers = fotoAchada.likers.filter(likerAtual => likerAtual.login !== liker.login);
-            fotoAchada.likers = novosLikers;
-        }
+            return { likeada, likers: novosLikers };
 
-        return state;
+        });
     }
 
     return state;
