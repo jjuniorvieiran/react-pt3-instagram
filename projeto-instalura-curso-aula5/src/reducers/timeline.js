69ba58554abbeeec18bee0e05dd861949eb90f11
Junior <junior.jaava@gmail.com>
2020-02-08 16:20:40 +0100

    Working with immatableJS. Change state from array to List immatable

--- projeto-instalura-curso-aula5/src/reducers/timeline.js
+++ projeto-instalura-curso-aula5/src/reducers/timeline.js
@@ -1,13 +1,24 @@
-export function timeline(state = [], action) { //initial state empty EC6
+import { List } from 'immutable';
+
+export function timeline(state = new List(), action) { //initial state empty EC6
     if (action.type === 'LISTAGEM') {
-        return action.fotos;
+        return new List(action.fotos);
     }
 
-    if(action.type === 'COMENTARIO'){
-        const fotoAchada = state.find(foto => foto.id === action.fotoId);
-        fotoAchada.comentarios.push(action.novoComentario);
+    if (action.type === 'COMENTARIO') {
+        const fotoEstadoAntigo = state.find(foto => foto.id === action.fotoId);
+        const novosComentarios = fotoEstadoAntigo.comentarios.concat(action.novoComentario);
     
-        return state;
+        //Nós queremos que ele preencha com todas propriedade que tem no fotoEstadoAtual e depois, que troque a propriedade comentarios 
+        //pelos novosComentarios. Sempre o último objeto literal, prevalecerá a que estava antes. Isto irá nos retornar o fotoEstadoNovo.
+        const fotoEstadoNovo = Object.assign({},fotoEstadoAntigo,{comentarios:novosComentarios});
+    
+        const indiceDaLista = state.findIndex(foto => foto.id === action.fotoId);
+        //O próximo passo será o adicionar a variável novaLista com o state.set() para trocarmos um elemento com determinado índice. 
+        //Incluiremos dois parâmetros: indiceDaLista e fotoEstadoNovo. No fim, será retornado uma novaLista.
+        const novaLista = state.set(indiceDaLista,fotoEstadoNovo);
+    
+        return novaLista;
     }
 
     if (action.type === 'LIKE') {
@@ -26,7 +37,7 @@ export function timeline(state = [], action) { //initial state empty EC6
         }
 
         return state;
-   }
+    }
 
     return state;
 } 
