ba4057dbb10d364ed971c96cfb89909ebb0cde6a
Junior <junior.jaava@gmail.com>
2020-02-10 19:24:46 +0100

    using react provider to make store in a global context

--- projeto-instalura-curso-aula5/src/App.js
+++ projeto-instalura-curso-aula5/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
-import Timeline from './componentes/Timeline'; 
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import {timeline} from './reducers/timeline';
-import {notificacao} from './reducers/header';  
-
-const reducers = combineReducers({timeline,notificacao});
-const store = createStore(reducers,applyMiddleware(thunkMiddleware));
+import Timeline from './componentes/Timeline';
 
 class App extends Component {
-  render() {    
+  render() {
     return (
-    <div id="root">
-      <div className="main">
-        <Header store={store}/>
-        <Timeline login={this.props.params.login} store={store}/>
+      <div id="root">
+        <div className="main">
+          <Header store={this.context.store} />
+          <Timeline login={this.props.params.login} store={this.context.store} />
+        </div>
       </div>
-    </div>
     );
   }
+}
+
+App.contextTypes = {
+  store: React.PropTypes.object.isRequired
 }
 
 export default App;
